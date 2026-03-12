45b0b3fe283c59ca2923bb03b6710bf8abd227b9
MD.RAYHAN <md.rayhanx617@gmail.com>
2026-03-13 01:20:06 +0600

    feat: add Firebase auth state observer with useEffect in AuthProvider

--- src/Context/AuthContext/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AuthContext } from '../AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../../firebase/firebase.config';
 
 const AuthProvider = ({children}) => {
@@ -9,10 +9,44 @@ const AuthProvider = ({children}) => {
     const  createUser =(email,password) =>{
         return createUserWithEmailAndPassword(auth,email,password)
     }
+    const signIn = (email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
 
+
+    }
+
+    // useEffect(()=> {},[])
+    // useEffect(()=>{
+    //     // step 1 : observer set 
+    //     // step 2 : set in a variable 
+    //     //step 3 : retrun and call  the varible so that you can clear the ref
+    // },[])
+    // GET CURRENT USER INFO //
+//     onAuthStateChanged(auth,(currentUser) =>{
+//        if(currentUser){
+//  console.log('inside observer if',currentUser)
+//     }
+       
+//        else{
+//         console.log('current user else',currentUser)
+//        }
+//     })
+
+useEffect(()=>{
+    // Sset the  observer 
+    const unsubscribe = onAuthStateChanged(auth,(currentUser) =>{
+        console.log('current user in auth state change', currentUser)
+    })
+    // CLAER THE OBSERVER ON UMMOUNT 
+    return ()=>{
+        unsubscribe();
+    }
+})
+   
 
     const authINfo = {
         createUser,
+        signIn
     }
     return (
         <div>
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,28 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import React, { use } from 'react';
 import { auth } from '../../firebase/firebase.config';
+import { AuthContext } from '../../Context/AuthContext/AuthContext';
 
 
 
 const googleProvider = new GoogleAuthProvider();
 const Login = () => {
+  const { signIn } = use(AuthContext)
+
+  const handleLogin = (e)=>{
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signIn(email,password)
+    .then(result =>{
+      const user = result.user;
+      console.log(user)
+    })
+    .catch(error =>{
+      console.log(error.message)
+    })
+  }
+ 
     // Google SignIn //
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth,googleProvider)
@@ -27,12 +44,12 @@ const Login = () => {
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <h1 className="text-3xl font-bold">Login now!</h1>
       <div className="card-body">
-       <form >
+       <form onSubmit={handleLogin} >
          <fieldset className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" className="input" name='email' placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" className="input" name='password' placeholder="Password" />
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Login</button>
             <button onClick={handleGoogleSignIn} className="btn btn-neutral mt-4">Login with Google</button>
